26e2c1b960cb0f24c5c566556bb5c883bc051214
Maloney Liu <maloneyl@users.noreply.github.com>
2017-12-29 18:29:10 +0000

    scaffold a project using vue-loader with vue-cli; add one component
    https://laracasts.com/series/learn-vue-2-step-by-step/episodes/16

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Another One</message>
+  </div>
+</template>
+
+<script>
+  import Message from './components/Message.vue';
+
+  export default {
+    name: 'app',
+    components: { Message },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+  .box {
+    background: #e3e3e3;
+    border: 1px solid #c5c5c5;
+    padding: 1em;
+    margin-bottom: 1em;
+  }
+</style>
