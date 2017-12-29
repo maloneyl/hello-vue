666999bebb31e723a431215010b7c1e600bdf08b
Maloney Liu <maloneyl@users.noreply.github.com>
2017-12-29 18:38:58 +0000

    add Counter as we try out hot module replacement
    * the state for all components remain changed when you update a component
    
    https://laracasts.com/series/learn-vue-2-step-by-step/episodes/17

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Count: {{ count }}</h2>
+
+    <button @click="count += 1">Increment</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
